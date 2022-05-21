9dc47f812db75f6b261717d3c216d2325c475cc3
chris <94062641+chrisprof@users.noreply.github.com>
2022-05-21 14:11:18 -0400

    fixed nav bar positioning

--- serv/src/components/Navbar/NavBar.js
+++ serv/src/components/Navbar/NavBar.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './NavBar.css'
 function NavBar() {
     return (
-        <nav>
-            <ul className='n-list'>
-                <li className='n-item'>
-                    <a href='#'>home</a>
-                </li>
-                <li className='n-item'>
-                    <a href='#'>settings</a>
-                </li>
-                <li className='n-item'>
-                    <a href='#'>about us</a>
-                </li>
-            </ul>
-        </nav>
+    <nav>
+        <ul className='n-list'>
+            <li className='n-item'>
+                <a href='#'>home</a>
+            </li>
+            <li className='n-item'>
+                <a href='#'>settings</a>
+            </li>
+            <li className='n-item'>
+                <a href='#'>about us</a>
+            </li>
+        </ul>
+    </nav>   
     )
 }
 
