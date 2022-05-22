5ccfe619067d07a2894891f3df42a53b7b174f4b
coolerzanu <prakshab.adhikari@gmail.com>
2022-05-22 07:05:27 -0400

    About me Finished

--- serv/src/pages/AboutUs.js
+++ serv/src/pages/AboutUs.js
@@ -2,15 +2,47 @@ import "./AboutUs.css"
 function AboutUs()
 {
     return(
-        <div >
+        <div>
             <h1 className="AboutUsHeader">about us</h1>
-            <h2>Developers</h2>
-            <div id="developers"></div>
-            <h2>Presentation Writer</h2>
-            <div id="developers"></div>
-            <h2>Graphic Designers</h2>
-            <h2>About our project - serv</h2>
-            <p>serv is a</p>
+            <div id="A-Container">
+                <div className='A-Child'>
+                    <img src='https://cdn.discordapp.com/avatars/974029801901928509/c74ac6979edf6d1a4ecd4dfccb2a4ddb.webp?size=160' alt='pfp' className="A-pfp-s"/>
+                    <h2 className='A-Names'>Chris123</h2>
+                    <p className='A-disc'>Chris mainly worked on the back and front end used his knowledge of firebase to hook everything up</p> <br/> <br/> <br/>
+                </div>
+                <p className='A-Breaker'></p> <br/> <br/>
+
+                <div className='A-Child'>
+                    <img src='https://cdn.discordapp.com/avatars/580477140156284929/f59ad800209f86efd6ccdcdf2ee9cc94.webp?size=256' alt='pfp' className="A-pfp-b"/>
+                    <h2 className='A-Names'>Coolerzau</h2>
+                    <p className='A-disc'>Prakshab Adhikari was born in Chitwan, Nepal, and raised in the United States. He's lived all across the US, ranging from Texas, Oklahoma, and New Jersey! He strives to be the best person he can be, no matter where he chooses to go. He was the main front end developer for this project and helped make a couple pages.</p> <br/> <br/> <br/>
+                </div>
+
+                <p className='A-Breaker'></p> <br/> <br/>
+
+                <div className='A-Child'>
+                    <img src='https://cdn.discordapp.com/avatars/954851075440136212/11e744862c28690a75a4349ad532383d.webp?size=256' alt='pfp' className="A-pfp-b"/>
+                    <h2 className='A-Names'>mujtaba</h2>
+                    <p className='A-disc'>Mujtaba is an avid coder and science researcher. His most recent project, "ALGAETECH," focused on utilizing microalgae from local Jersey City freshwater reservoirs to treat the extending wastewater sewage issue in the Hudson River. It was awarded First Place in the 2022 Jersey City Science Fair Challenge, and he is currently researching more methods to implement his topic at the Junior Academy of the New York Academy of Arts and Sciences. He is a member of the Citywide Student Council for the Jersey City Board of Education, has been awarded the Hudson County Executive's Award of Excellence four times and won a Gold Medal in the 2022 National Scholastic Art and Writing Awards for his poetry collection</p> <br/> <br/> <br/>
+                </div>
+
+                <p className='A-Breaker'></p> <br/> <br/>
+
+                <div className='A-Child'>
+                    <img src='https://cdn.discordapp.com/avatars/760156808403877949/648ab671b39d4918e218a83ba9b6f346.webp?size=160' alt='pfp' className="A-pfp-s"/>
+                    <h2 className='A-Names'>lakishad13</h2>
+                    <p className='A-disc'>Lakshya is a beginner at HTML, CSS, and Rect.js and enjoys learning how to code in his free time. During the creation of this website, he ran into multiple errors that were quickly resolved with the help of his fellow teammates.</p> <br/> <br/> <br/>
+                </div>
+
+                <p className="A-Breaker"></p> <br/> <br/>
+
+                <div className='A-Child'>
+                <img src='https://media.discordapp.net/attachments/976552609156575234/977581532556443668/Screen_Shot_2022-05-21_at_10.39.54_AM.png?width=671&height=594' alt='pfp' id="A-Main-pfp"/>
+                    <h2 className='A-Names'>About our project - serv</h2> 
+                    <p className='A-disc'>Serv is an original, community-oriented website where students can find & reserve community service opportunities at their school. Educators and officials from schools can post these opportunities on Serv, logging in with Google and listing that community service opportunity at the school of their choice, for students to then reserve & serve — all from the leisure of their homes. Students can view community service opportunities that have been listed by their school’s educators available using the drop-down menu, select the school of their choice, & reserve the spot to serve. This provides a much better experience for all three parties; it eliminates all prior human contact and lets educators scout all types of students. Serv would be managed by an authoritative figure in schools such as, the guidance counselor or the Student Council. To add on, one reliable student may be selected to check Serv every morning and update a bulletin board in high traffic areas around the school. This will provide equal opportunity for students without technology at home. To illustrate, if you attend High Tech High School, and an educator at the Design & Fabrication Academy is in the severe need of volunteers to aid in an initiative — the educator can simply log onto Serv, select High Tech as the school of their choice via the drop-down menu, & list the community service opportunity. From there, a student at High Tech, whether in the need of service hours for graduation or infatuation purposes, can log onto Serv, select High Tech as the school of their choice via the drop-down menu, & can view & reserve that community service opportunity for themselves.
+</p>
+                </div>
+            </div>
         </div>
     )
 }
